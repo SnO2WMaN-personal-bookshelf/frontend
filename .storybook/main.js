624074c6ddc188108c3d8bf7875bf5f5210062e7
SnO2WMaN <sno2wman.official@gmail.com>
2020-10-30 09:58:58 +0900

    chore: :gear: Storybookがtsconfig.jsonのpathsを解決しないのを修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,18 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  // eslint-disable-next-line require-await
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+    ];
+
+    return config;
+  },
 };
