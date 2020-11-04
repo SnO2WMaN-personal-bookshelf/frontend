9fd4132f267611dbfba69936c6ae7a3adc28626b
SnO2WMaN <sno2wman.official@gmail.com>
2020-11-05 00:27:09 +0900

    chore: :art: Storybookの設定更新

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
-import './tailwind.css';
+import React from 'react';
+
+import '../src/styles/tailwind.css';
+import {ConfiguratedI18nextProvider} from '../src/i18n';
+
+export const decorators = [
+  (Story) => (
+    <ConfiguratedI18nextProvider>
+      <Story />
+    </ConfiguratedI18nextProvider>
+  ),
+];
 
 export const parameters = {
   actions: {
