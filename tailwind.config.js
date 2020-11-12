113c898df97df81b4846176b85ca8c451d6068a9
SnO2WMaN <sno2wman.official@gmail.com>
2020-11-13 01:47:29 +0900

    chore: :gear: tailwind.config.js更新

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,25 @@ module.exports = {
   },
   theme: {
     extend: {
+      inset: {
+        100: '100%',
+      },
+      height: {
+        // n/2
+        'screen-1/2': `${(100 / 2) * 1}vh`,
+        'screen-2/2': `${(100 / 2) * 2}vh`,
+        // n/4
+        'screen-1/4': `${(100 / 4) * 1}vh`,
+        'screen-2/4': `${(100 / 4) * 2}vh`,
+        'screen-3/4': `${(100 / 4) * 3}vh`,
+        'screen-4/4': `${(100 / 4) * 4}vh`,
+        // n/5
+        'screen-1/5': `${(100 / 5) * 1}vh`,
+        'screen-2/5': `${(100 / 5) * 2}vh`,
+        'screen-3/5': `${(100 / 5) * 3}vh`,
+        'screen-4/5': `${(100 / 5) * 4}vh`,
+        'screen-5/5': `${(100 / 5) * 5}vh`,
+      },
       spacing: {
         [18 * 4]: `${18}rem` /* 72 */,
         [21 * 4]: `${21}rem` /* 84 */,
@@ -18,6 +37,8 @@ module.exports = {
       },
     },
   },
-  variants: {},
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+  },
   plugins: [],
 };
