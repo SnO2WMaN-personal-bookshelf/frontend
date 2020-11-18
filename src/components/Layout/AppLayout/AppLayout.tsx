46dc505bfc30c1fb8f19ffe414d414cf580fc654
SnO2WMaN <sno2wman.official@gmail.com>
2020-11-18 23:49:11 +0900

    feat: :sparkles: Navbarとページ部分で最低100vhあるように設定

--- src/components/Layout/AppLayout/AppLayout.tsx
+++ src/components/Layout/AppLayout/AppLayout.tsx
@@ -9,10 +9,14 @@ export type ContainerProps = {
 export const Container: React.FC<ContainerProps> = ({children, className}) => {
   return (
     <div className={clsx(className, 'flex', 'flex-col', 'bg-gray-100')}>
-      <Navbar className={clsx('w-full', 'sticky', 'top-0', 'left-0', 'z-50')} />
-      <PageLayout className={clsx('w-full', 'h-full', 'my-8')}>
-        {children}
-      </PageLayout>
+      <div className={clsx('min-h-screen', 'flex', 'flex-col')}>
+        <Navbar
+          className={clsx('w-full', 'sticky', 'top-0', 'left-0', 'z-50')}
+        />
+        <PageLayout className={clsx('w-full', 'h-full', 'flex-grow', 'my-8')}>
+          {children}
+        </PageLayout>
+      </div>
       <footer
         className={clsx('w-full', 'h-full', 'bg-blue-400', 'flex-grow')}
       />
